3db995a4276b973f3848518008a57097784dd6fd
Purushottam-BCA <84143197+Purushottam-BCA@users.noreply.github.com>
2021-05-22 14:00:48 +0530

    Create 2. Bubble Sort.cpp

---
+++ 1. Sorting/2. Bubble Sort.cpp
@@ -0,0 +1,24 @@
+// Time Complexity ->  Best Case :: O(n)    Worst case :: O(N^2)
+
+void swap( int &a, int &b)
+{
+  a=a+b;
+  b=a-b;
+  a=a-b;
+}
+
+void Bubble_sort( int a[], int n )
+{
+  for( int i=0; i<n-1 ; i++ )
+  {
+    bool ischanged=false;
+    for( int j=1; j<n-i-1; j++)
+      if(a[i]>a[j])
+      {
+        swap(a[i],a[j]);
+        ischanged=true;
+      }
+    if(!ischanged)
+      return;
+  }
+}
