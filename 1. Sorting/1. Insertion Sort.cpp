5557d3d139872621b6c2bb5e62b5a59b53575b4b
Purushottam-BCA <84143197+Purushottam-BCA@users.noreply.github.com>
2021-05-22 13:54:28 +0530

    Create 1. Insertion Sort.cpp

---
+++ 1. Sorting/1. Insertion Sort.cpp
@@ -0,0 +1,17 @@
+// Time Complexity :: Best case --> O(N)    Worst case --> O(N^2)
+
+void Insertion_Sort( int a[], int size)
+{
+  int key,j;
+  for(int i=1; i<size; i++)
+  {
+    key = a[i];
+    j = i-1;
+    while(j>0 && a[j]>key)
+    {
+      a[j+1] = a[j];
+      j--;
+    }
+    a[j+1] = key;
+  }
+}
